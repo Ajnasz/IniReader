ce62c007c2c40873f2a71a7cb5ca7ac06dbb2b33
Lajos Koszti <ajnasz@ajnasz.hu>
2011-05-18 20:23:59 +0200

    Make possible to use async parsing

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
+/*jslint indent: 2*/
+/*globals require: true*/
 
-/**
- * A simple function to get the file content
- * line by line
- */
-var getLines = function(file) {
-  var fs, fd, pos, lines, line;
-
-  fs = require('fs');
-  fd = fs.openSync(file, 0400);
-  pos = 0;
-  lines = [];
-
-  var getLine = function() {
-    var char,
-        line = '';
-
-    do {
-      char = fs.readSync(fd, 1, pos);
-      line += char[0];
-      pos += 1;
-    } while(char[0] !== '\n' && char[0] !== '\r' && char[1]);
-
-    return line;
+var getLines = function (file, cb, async) {
+  var fs = require('fs'), splitLines, data;
+  splitLines = function (data) {
+    data = data.toString();
+    var lines;
+    if (data.indexOf('\r\n') > -1) {
+      lines = data.split('\r\n');
+    } else if (data.indexOf('\n') > -1) {
+      lines = data.split('\n');
+    } else if (data.indexOf('\r') > -1) {
+      lines = data.split('\r');
+    } else { // mostly it's only one line
+      lines = [data];
+    }
+    return lines.filter(function (line) {
+      return line !== '';
+    });
   };
-
-  do {
-    line = getLine();
-    lines.push(line);
-  } while(line !== '');
-  fs.closeSync(fd);
-
-  return lines;
+  if (async) {
+    fs.readFile(file, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      cb(splitLines(data));
+    });
+  } else {
+    data = fs.readFileSync(file);
+    return splitLines(data);
+  }
 };
 
 /**
@@ -44,10 +43,10 @@ var getLines = function(file) {
  * FIXME
  * is it a bug?
  */
-var fixQuoted = function(str) {
-  if(
-    (str[0] == '"' && str[str.length - 1] == '"') ||
-    (str[0] == "'" && str[str.length - 1] == "'")
+var fixQuoted = function (str) {
+  if (
+    (str[0] === '"' && str[str.length - 1] === '"') ||
+    (str[0] === "'" && str[str.length - 1] === "'")
   ) {
 
     return str.substr(1, str.length - 2);
@@ -63,131 +62,143 @@ var fixQuoted = function(str) {
  * @class IniReader
  * @constructor
  */
-var IniReader = function(file) {
-  this.lines = getLines(file);
-  this.values = this.parseFile();
+var IniReader = function (file, async) {
+  this.async = !!async;
+  this.file = file;
+};
+require('util').inherits(IniReader, require('events').EventEmitter);
+/**
+  * Regexp to get the group names
+  */
+IniReader.prototype.groupRex = /^\s*\[\s*([^\]]+)\s*\]$/;
+
+/**
+  * Regexp to get key/value pairs
+  */
+IniReader.prototype.keyValueRex = /^\s*([^=]*\w)\s*=\s*(.*)\s*$/;
+
+IniReader.prototype.init = function () {
+  if (this.async) {
+    getLines(this.file, function (lines) {
+      this.lines = lines;
+      this.values = this.parseFile();
+      this.emit('fileParse');
+    }.bind(this), true);
+  } else {
+    this.lines = getLines(this.file);
+    this.values = this.parseFile();
+    this.emit('fileParse');
+  }
 };
 
-IniReader.prototype = {
-  /**
-   * Regexp to get the group names
-   */
-  groupRex: /^\s*\[\s*([^\]]+)\s*\]$/,
+/**
+  * Tries to find a group name in a line
+  * @method groupMatch
+  * @type {String|False}
+  * @returns the group name if found or false
+  */
+IniReader.prototype.groupMatch = function (line) {
+  var groupMatch = line.match(this.groupRex);
+  return groupMatch ? groupMatch[1] : false;
+};
 
-  /**
-   * Tries to find a group name in a line
-   * @method groupMatch
-   * @type {String|False}
-   * @returns the group name if found or false
-   */
-  groupMatch: function(line) {
-    var groupMatch = line.match(this.groupRex);
-    return groupMatch ? groupMatch[1] : false;
-  },
+/**
+  * Tries to find a key/value pair in a line
+  * @method groupMatch
+  * @type {Object|False}
+  * @returns the key value pair in an object ({key: 'key', value;'value'}) if found or false
+  */
+IniReader.prototype.keyValueMatch = function (line) {
+  var keyValMatch = line.match(this.keyValueRex);
+  return keyValMatch ? {key: keyValMatch[1], value: keyValMatch[2]} : false;
+};
 
-  /**
-   * Regexp to get key/value pairs
-   */
-  keyValueRex: /^\s*([^=]*\w)\s*=\s*(.*)\s*$/,
+/**
+  * Parses an init file, and extracts blocks with keys and values
+  * @method parseFile
+  * @returns the conf tree
+  * @type Object
+  */
+IniReader.prototype.parseFile = function () {
 
-  /**
-   * Tries to find a key/value pair in a line
-   * @method groupMatch
-   * @type {Object|False}
-   * @returns the key value pair in an object ({key: 'key', value;'value'}) if found or false
-   */
-  keyValueMatch: function(line) {
-    var keyValMatch = line.match(this.keyValueRex);
-    return keyValMatch ? {key: keyValMatch[1], value: keyValMatch[2]} : false;
-  },
+  var output, lines, skipLineRex, chompRex, trimRex, nonWhitespaceRex,
+      groupName, keyVal, line, currentSection, lineNumber;
 
-  /**
-   * Parses an init file, and extracts blocks with keys and values
-   * @method parseFile
-   * @returns the conf tree
-   * @type Object
-   */
-  parseFile: function() {
+  output = {};
+  lines = this.lines;
 
-    var output, lines, skipLineRex, chompRex, trimRex, nonWhitespaceRex,
-        groupName, keyVal, line, currentSection, lineNumber;
+  // regular expressions to clear, validate and get the values
+  skipLineRex = /^\s*(\n|\#|;)/;
+  chompRex = /(?:\n|\r)$/;
+  trimRex = /^\s+|\s+$/g;
+  nonWhitespaceRex = /\S/;
 
-    output = {};
-    lines = this.lines;
+  lineNumber = 0;
 
-    // regular expressions to clear, validate and get the values
-    skipLineRex = /^\s*(\n|\#|;)/ ;
-    chompRex = /(?:\n|\r)$/;
-    trimRex = /^\s+|\s+$/g;
-    nonWhitespaceRex = /\S/;
+  while (line = lines.shift()) {
 
-    lineNumber = 0;
-
-    while(line = lines.shift()) {
-
-      lineNumber += 1;
-      // skip comments and empty lines
-      if(skipLineRex.test(line) || !nonWhitespaceRex.test(line)) {
-        continue;
-      }
-
-      line = line.replace(chompRex, '');
-      line = line.replace(trimRex, '');
-
-      // block name
-      groupName = this.groupMatch(line);
-      if(groupName) {
-        currentSection = groupName;
-        if(!output[currentSection]) {
-          output[currentSection] = {};
-        }
-        continue;
-      }
-
-      // key/value pairs
-      keyVal = this.keyValueMatch(line);
-      if(keyVal) {
-        if(currentSection) {
-          output[currentSection][keyVal.key] = fixQuoted(keyVal.value);
-          continue;
-        }
-      }
-
-      // if we came this far, the syntax couldn't be validated
-      throw new Error("syntax error in line " + lineNumber);
-
-    };
-    return output;
-
-  },
-  /**
-   * @method getBlock
-   * @returns A block of the conf tree
-   * @type Object
-   */
-  getBlock: function(block) {
-    return typeof block == 'string' ?
-           this.values[block] :
-           this.values;
-  },
-  /**
-   * @method getValue
-   * @returns the value of the key
-   * @param String block The name of the block where the key should be defined
-   * @param String key The name of the key which value should be returned
-   */
-  getValue: function(block, key) {
-
-    if(typeof(block) != 'string') {
-      throw new Error('block is not a string');
+    lineNumber += 1;
+    // skip comments and empty lines
+    if (skipLineRex.test(line) || !nonWhitespaceRex.test(line)) {
+      continue;
     }
 
-    var sec = this.getBlock(block);
+    line = line.replace(chompRex, '');
+    line = line.replace(trimRex, '');
 
-    if(sec === this.values || typeof(sec) == 'undefined') {
-      throw new Error('block ' + block + ' is undefined');
+    // block name
+    groupName = this.groupMatch(line);
+    if (groupName) {
+      currentSection = groupName;
+      if (!output[currentSection]) {
+        output[currentSection] = {};
+      }
+      continue;
     }
-    return sec[key];
+
+    // key/value pairs
+    keyVal = this.keyValueMatch(line);
+    if (keyVal) {
+      if (currentSection) {
+        output[currentSection][keyVal.key] = fixQuoted(keyVal.value);
+        continue;
+      }
+    }
+
+    // if we came this far, the syntax couldn't be validated
+    throw new Error("syntax error in line " + lineNumber);
+
   }
+  return output;
+
+};
+/**
+  * @method getBlock
+  * @returns A block of the conf tree
+  * @type Object
+  */
+IniReader.prototype.getBlock = function (block) {
+  return typeof block === 'string' ?
+          this.values[block] :
+          this.values;
+};
+/**
+  * @method getValue
+  * @returns the value of the key
+  * @param String block The name of the block where the key should be defined
+  * @param String key The name of the key which value should be returned
+  */
+IniReader.prototype.getValue = function (block, key) {
+
+  if (typeof(block) !== 'string') {
+    throw new Error('block is not a string');
+  }
+
+  var sec = this.getBlock(block);
+
+  if (sec === this.values || typeof(sec) === 'undefined') {
+    throw new Error('block ' + block + ' is undefined');
+  }
+  return sec[key];
 };
 exports.IniReader = IniReader;
